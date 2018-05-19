03f2e45ec4f4ea3e2a4f2490e04d03c8e3ef7692
trungtx92 <31231452+trungtx92@users.noreply.github.com>
2018-05-19 17:05:53 +1000

    interface

--- Mininet/src/View/MainMenuGUI.java
+++ Mininet/src/View/MainMenuGUI.java
@@ -1,10 +1,114 @@
 package View;
 
-public class MainMenuGUI {
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import imageProcess.ImagePro;
+
+public class MainMenuGUI extends JFrame implements ActionListener {
+	
+	JPanel pnMenu = new JPanel();
+	JPanel pnChanging = new JPanel();
+	JButton btAddPerson = new JButton("Add Person");
+	JButton btSelectPerson = new JButton("Select Person");
+	JButton btRelationship = new JButton("RelationShip");
+	JButton btFindoutRelations = new JButton("Find out");
+	JButton btExit = new JButton("Exit");
+	JLabel lbBackground = new JLabel("");
+	ImagePro imagePro = new ImagePro();
+	int _btWidth = 120;
+	int _btHeight = 35;
+	int _btX = 10;
+	int _btAddPersonY = 50;
+	int _gap = 30;
+	int _frameWidth = 700;
+	int _frameHeight = 600;
+	int _frameX = 400;
+	int _frameY = 150;
+	int _pnMenuWidth = 150;
+	int _pnChangingWidth = 550;
+	
+	public MainMenuGUI() {
+		setFrame();
+	}
+	public void setFrame() {
+		this.setLayout(null);
+		pnMenu.setLayout(null);
+		pnChanging.setLayout(new CardLayout());
+		btAddPerson.setBounds(_btX, _btAddPersonY, _btWidth, _btHeight);
+		btSelectPerson.setBounds(_btX, _btAddPersonY + _btHeight + _gap, _btWidth, _btHeight);
+		btRelationship.setBounds(_btX, btSelectPerson.getY() + _btHeight + _gap, _btWidth, _btHeight);
+		btFindoutRelations.setBounds(_btX, btRelationship.getY() + _btHeight + _gap, _btWidth, _btHeight);
+		btExit.setBounds(_btX, btFindoutRelations.getY() + _btHeight + _gap, _btWidth, _btHeight);
+		pnMenu.setSize(_pnMenuWidth, _frameHeight);
+		pnChanging.setBounds(_pnMenuWidth, 0, _pnChangingWidth, _frameHeight);
+		lbBackground.setSize(_btWidth, _btHeight);
+		lbBackground.setIcon(imagePro.iMovieContent);
+		pnMenu.setBackground(Color.BLACK);
+		pnChanging.setBackground(Color.BLUE);
+		btExit.addActionListener(this);
+		btSelectPerson.addActionListener(this);
+		btRelationship.addActionListener(this);
+		btFindoutRelations.addActionListener(this);
+		btAddPerson.addActionListener(this);
+		pnMenu.add(btAddPerson);
+		pnMenu.add(btSelectPerson);
+		pnMenu.add(btRelationship);
+		pnMenu.add(btFindoutRelations);
+		pnMenu.add(btExit);
+		pnChanging.add(lbBackground);
+		this.setBounds(_frameX, _frameY,_frameWidth, _frameHeight);
+		this.add(pnMenu);
+		this.add(pnChanging);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new MainMenuGUI();
+		
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == btAddPerson) {
+			
+		}
+		if(e.getSource() == btSelectPerson) {
+			pnChanging.removeAll();
+			pnChanging.add(new PnSelectPersonGUI(this));
+			pnChanging.repaint();
+			pnChanging.revalidate();
+		}
+		if(e.getSource() == btRelationship) {
+			pnChanging.removeAll();
+			pnChanging.add(new PnRelationshipGUI());
+			pnChanging.repaint();
+			pnChanging.revalidate();
+		}
+		if(e.getSource() == btFindoutRelations) {
+			pnChanging.removeAll();
+			pnChanging.add(new PnFindoutRelations());
+			pnChanging.repaint();
+			pnChanging.revalidate();
+			
+		}
+		if(e.getSource() == btExit) {
+			System.exit(0);
+		}
 	}
 
 }
+
+
+
+
---
+++ Mininet/src/View/PnFindoutRelations.java
@@ -0,0 +1,88 @@
+package View;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+public class PnFindoutRelations extends JPanel implements ActionListener, MouseListener {
+	JScrollPane spProfileList = new JScrollPane();
+	JTable tbProfileList = new JTable();
+	TableModel dataModel = null;
+	JButton btCancel = new JButton("Cancel");
+	int _btWidth = 100;
+	int _btHeight = 40;
+	int _btX = 400;
+	int _btY = 500;
+	int _spWidth = 450;
+	int _spHeight = 420;
+	int _spX = 50;
+	int _spY = 50;
+	MainMenuGUI mainMenu;
+	public PnFindoutRelations() {
+		// TODO Auto-generated constructor stub
+		setPanel();
+		this.mainMenu = mainMenu;
+	}
+	public void setPanel() {
+		this.setLayout(null);
+		this.setBackground(Color.GRAY);
+//		btCancel.setBounds(_btX, _btY, _btWidth, _btHeight);
+		spProfileList.setBounds(_spX, _spY, _spWidth, _spHeight);
+		spProfileList.getViewport().add(tbProfileList);
+		tbProfileList.addMouseListener(this);
+		loadDataModel();
+//		this.add(btCancel);
+		this.add(spProfileList);
+	}
+	public Object[] loadColumnName() {
+		return new Object[] {"Name", "Age"};
+	}
+	public Object[][] loadRowData(){
+		return new Object[][] {{"trung", "12"}, {"Kevin", "14"}};
+	}
+	public void loadDataModel() {
+		dataModel = new DefaultTableModel(loadRowData(), loadColumnName());
+		tbProfileList.setModel(dataModel);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "This person done have any relation!");
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
